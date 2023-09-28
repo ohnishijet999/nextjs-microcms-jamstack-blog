392bb587efdce2e041c7e3eba8c957d6acb08d22
Kaoru Onishi <kaoruohnishi03023@gmail.com>
2023-09-28 18:08:05 +0900

    フッター追加

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export function Footer () {
   return (
-    <footer className="w-full h-20 bg-teal-400 flex justify-center items-center">
-      <p className="text-white">Copyright © 2023 K.O. All Rights Reserved.</p>
+    <footer className="w-full h-40 bg-teal-400 flex justify-center items-center">
+      <div>
+        <div className="flex gap-3 justify-center">
+          <Link href="https://github.com/ohnishijet999" target="_blank">
+            <Image src="/images/github-mark.svg" width={40} height={40} alt="GitHub" className="hover:opacity-70"/>
+          </Link>
+          <Link href="mailto:onishi.kaoru.1999@gmail.com" target="_blank">
+            <Image src="/images/mail.svg" width={40} height={40} alt="メールアドレス" className="hover:opacity-70"/>
+          </Link>
+        </div>
+        <p className="text-white mt-3">Copyright © 2023 K.O. All Rights Reserved.</p>
+      </div>
     </footer>
   )
 }
