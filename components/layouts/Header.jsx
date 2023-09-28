5aba84db360a00c0ebfdb55099aa242b9ea513a8
Kaoru Onishi <kaoruohnishi03023@gmail.com>
2023-09-28 17:44:08 +0900

    文言変更

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Header () {
   return (
     <header className="h-24 w-full flex justify-center items-center bg-[url('/images/header.jpg')] px-3">
-      <h1 className="text-white text-3xl font-black">🚢Webエンジニアブログ🌊</h1>
+      <h1 className="text-white text-3xl font-black">🚢Webエンジニアブログ🔩</h1>
     </header>
   )
 }
